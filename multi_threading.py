8802b25fb9b3f01f88057ffa916c9dbe358ba5a5
marcin <m.stolar2001@gmail.com>
2024-11-11 21:21:55 +0100

    WPI

--- multi_threading.py
+++ multi_threading.py
@@ -4,6 +4,7 @@ import time
 import logging
 from concurrent.futures import ThreadPoolExecutor
 import threading
+from queue import Queue
 
 logging.basicConfig(level='INFO')
 titles = []
@@ -33,8 +34,32 @@ def download_all_sites(sites):
                 executor.submit(download_site, *arg)
 
 
-if __name__ == "__main__":
+def demo():
     sites = [f"https://wsei.edu.pl/wydarzenia/lista/strona/{i}/?eventDisplay=past" for i in range(1, 11)]
     start = time.time()
     download_all_sites(sites)
     logging.info(f"Pobrano {len(titles)} tytulow in {time.time() - start} seconds")
+
+
+def get_cat_fact():
+    url = 'https://catfact.ninja/fact'
+    return requests.get(url).json()
+
+
+def task1(facts_num):
+    with ThreadPoolExecutor(20) as executor:
+        cat_facts = list(executor.map(lambda _: get_cat_fact(), range(facts_num)))
+    return print(cat_facts)
+
+def task2():
+    q = Queue(2)
+    products_counter = 0
+    def producent():
+        pass
+    def consument():
+        pass
+
+
+if __name__ == "__main__":
+    # demo()
+    task1(20)
